fe57232f911450d8130c53506fcb23b11e20ae6c
Louie Tolentino <silvercloud.0503@gmail.com>
2023-03-10 07:29:32 +0000

    create: All properties page

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,4 +1,4 @@
-import { useList } from '@pankod/refine-core'
+import { useTable } from '@pankod/refine-core'
 import { Add } from '@mui/icons-material'
 import { Box, Stack, Typography } from '@pankod/refine-mui'
 import { useNavigate } from '@pankod/refine-react-router-v6'
@@ -7,12 +7,32 @@ import { PropertyCard, CustomButton } from 'components'
 
 const AllProperties = () => {
   const navigate = useNavigate();
+
+  const { tableQueryResult: {data, isLoading, isError} } = useTable();
+  const allProperties = data?.data ?? [];  
+  
+  if(isLoading) return <Typography>Loading...</Typography>
+  if(isError) return <Typography>Error...</Typography>
+
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Typography fontSize={25} fontWeight={700} color="#11142D">All Properties</Typography>
         <CustomButton title="Add Property" handleClick={() => navigate('/properties/create')} backgroundColor="#475BE8" color="#FCFCFC" icon={<Add />} />
       </Stack>
+
+      <Box mt="20px" sx={{ display: 'flex', flexWrap: 'wrap', gap: 3}}>
+        {allProperties.map((property) => (
+          <PropertyCard
+            key={property._id}
+            id={property._id}
+            title={property.title}
+            price={property.price}
+            location={property.location}
+            photo={property.photo}
+          />
+        ))}
+      </Box>
     </Box>
   )
 }
